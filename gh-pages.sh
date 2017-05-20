2e84465802500d6b500e3a1cf891a75fd0bef9d7
Jacky (Jie) Liu <jackyjieliu@gmail.com>
2017-05-20 10:31:33 -0700

    update gh-pages script

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,1 +1,1 @@
-npm run build && cd ../../github/jsonpar.se/ && rm index.html && rm -rf static/ && cp -R ../../gitlab/json-board-rx/build/ .
+npm run build && cd ../../github/jsonpar.se/ && rm index.html && rm -rf static/ && cp -R ../../gitlab/json-board-rx/build/ . && git add .
